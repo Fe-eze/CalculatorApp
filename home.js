68637abb2c7334f5fd43e23615233db0049b58f4
Fe-eze Anya <fenobus@gmail.com>
2018-07-05 13:09:37 +0100

    Create home.js
    Initial commit
    Made simple layout in html
    Not sure if i'll use bootstrap for this

---
+++ home.js
@@ -0,0 +1,46 @@
+<html>
+<head>
+  <title>Fe-eze's Electronic Calculator</title>
+</head>
+<body>
+  <div class="container"> 
+    <p>Fe-eze's</p>
+    <p>Electronic Calculator</p>
+    <form oninput="x.value=29+parseInt(a.value)">
+    <input type="number" id="a" value=50>
+    <output name="screen" for="a"></output>
+    </form>
+    
+    <div>
+      <div class="row">
+        <input type="button" value='AC'>
+        <input type="button" value='CE'>
+        <input type="button" value='%'>
+        <input type="button" value='/'>
+      </div>
+      <div class="row">
+        <input type="button" value='7'>
+        <input type="button" value='8'>
+        <input type="button" value='9'>
+        <input type="button" value='X'>
+      </div>
+      <div class="row">
+        <input type="button" value='4'>
+        <input type="button" value='5'>
+        <input type="button" value='6'>
+        <input type="button" value='-'>
+      </div>
+      <div class="row">
+        <input type="button" value='1'>
+        <input type="button" value='2'>
+        <input type="button" value='3'>
+        <input type="button" value='+'>
+      </div>
+      <input type="button" value='0'>
+      <input type="button" value='.'>
+      <input type="button" value='='>
+      
+    </div>
+  </div>
+</body>
+</html>
